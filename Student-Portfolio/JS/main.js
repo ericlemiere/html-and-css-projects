16c99d1827d15df692f45866a0d7b2a055fb2943
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-06-05 22:07:12 -0700

    Portfolio Website updates

---
+++ Student-Portfolio/JS/main.js
@@ -0,0 +1,10 @@
+// ======= FUNCTION TO OPEN AND CLOSE CONTACT FORM ========
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
