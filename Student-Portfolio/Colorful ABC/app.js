a038e113f9f201e528315b08ab5ef6dc27e6ac40
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2022-11-08 13:13:40 -0800

    colorful abc updates for deployment

--- Student-Portfolio/Colorful ABC/app.js
+++ Student-Portfolio/Colorful ABC/app.js
@@ -1,18 +1,10 @@
-// $(document).ready(function()
-// {
-//     $(".swatch").mouseover(function()
-//     {
-//         var val = $("data-swatch-num");
-//         console.log(val);
-//         var x = document.getElementById("th2");
-//         x.innerHTML = $(this).text();
-//     });
-//     $(".swatch").mouseleave(function()
-//     {
-//         var x = document.getElementById("th2");
-//         x.innerHTML = "";
-//     });
-// });
+let isTouchScreen = false;
+if ("ontouchstart" in document.documentElement)
+{
+    isTouchScreen = true;
+}
+
+
 
 $(document).ready(() => {
     $("#banner").fadeIn(2500);
@@ -35,15 +27,30 @@ $(document).ready(function(){
 });
 
 
-
+// This is the onclick function
 function showColor(id) {
-    $(document).ready(function()
-    {
-        var color = $(".swatch")[id];
-        var x = document.getElementById(`th-${id}`);
-        x.innerHTML = $(color).text();
-        $("h3").fadeIn();
-    });
+    if (isTouchScreen) {
+        $(document).ready(function()
+        {
+            var color = $(".swatch")[id];
+            var x = document.getElementById(`th-${id}`);
+            x.innerHTML = "";
+            x.innerHTML = $(color).text();
+            $("h3").fadeIn();
+        });
+    }  
+}
+
+function showColorHover(id) {
+    if (!isTouchScreen) {
+        $(document).ready(function()
+        {
+            var color = $(".swatch")[id];
+            var x = document.getElementById(`th-${id}`);
+            x.innerHTML = $(color).text();
+            $("h3").fadeIn();
+        });
+    }
 }
 
 
@@ -52,21 +59,6 @@ function hideColor(id) {
     document.getElementById(`th-${id}`).innerHTML = "";
 }
 
-
-
-// $(window).on("load",function() {
-//     $(window).scroll(function() {
-//       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-//       $(".row").each(function() {
-//         /* Check the location of each desired element */
-//         var objectBottom = $(this).offset().top + $(this).outerHeight();
-        
-//         /* If the element is completely within bounds of the window, fade it in */
-//         if (objectBottom < windowBottom + 500) { //object comes into view (scrolling down)
-//           if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
-//         } else { //object goes out of view (scrolling up)
-//           if ($(this).css("opacity")==1) {$(this).fadeTo(800,0);}
-//         }
-//       });
-//     }).scroll(); //invoke scroll-handler on page-load
-// });
+function NavBarClickable() {
+    document.getElementById("NavBar").style.pointerEvents = "auto";
+}
