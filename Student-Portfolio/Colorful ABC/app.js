1e3bbd3f5ff4f57317e3461b163595770b794c60
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2022-11-09 22:21:46 -0800

    updates

--- Student-Portfolio/Colorful ABC/app.js
+++ Student-Portfolio/Colorful ABC/app.js
@@ -1,8 +1,16 @@
 let isTouchScreen = false;
-if ("ontouchstart" in document.documentElement)
-{
-    isTouchScreen = true;
-}
+$(document).ready(() => {
+    if ("ontouchstart" in document.documentElement)
+    {
+        document.getElementById("browser").innerHTML = "Click on the blocks of color to reveal the color name!";
+        isTouchScreen = true;
+    }
+    else
+    {
+        document.getElementById("browser").innerHTML = "Hover over the blocks of color to reveal the color name!";
+    }
+})
+
 
 
 
@@ -10,33 +18,44 @@ $(document).ready(() => {
     $("#banner").fadeIn(2500);
 })
 
-$(document).ready(function() {
-    $("#gotIt").mouseover(function() {
-        document.getElementById("gotIt").innerHTML = "Let's Go!";
-    });
-    $("#gotIt").mouseleave(function() {
-        document.getElementById("gotIt").innerHTML = "Ready?";
-    });
-});
+// $(document).ready(function() {
+//     $("#gotIt").mouseover(function() {
+//         document.getElementById("gotIt").innerHTML = "Let's Go!";
+//     });
+//     $("#gotIt").mouseleave(function() {
+//         document.getElementById("gotIt").innerHTML = "Ready?";
+//     });
+// });
 
 $(document).ready(function(){
-    $("#gotIt").click(function(){
-      $("#mainDiv").fadeIn();
-      $("#banner").fadeOut();
+    $("#gotIt").click(function() {
+        btn = document.getElementById("gotIt");
+        btn.innerHTML = "Let's Go!";
+        
+        setTimeout(() => {
+            $("#mainDiv").fadeIn("slow");
+            $("#banner").fadeOut("slow");
+        }, 700);
+      
     });
 });
 
 
 // This is the onclick function
+let textFadeOut;
 function showColor(id) {
     if (isTouchScreen) {
         $(document).ready(function()
         {
+            clearTimeout(textFadeOut);
             var color = $(".swatch")[id];
             var x = document.getElementById(`th-${id}`);
             x.innerHTML = "";
             x.innerHTML = $(color).text();
             $("h3").fadeIn();
+            textFadeOut = setTimeout(() => {
+                $("h3").fadeOut();
+            }, 2000);
         });
     }  
 }
